4bf24c411a826ecf1de024b82855e4f9b6995325
rdtmaster <hakeemalbahr@gmail.com>
2026-01-28 20:33:46 +0300

    temp test app for me

--- lib/main.dart
+++ lib/main.dart
@@ -1,364 +1,92 @@
-import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
-import 'package:path_provider/path_provider.dart';
 import 'dart:io';
-import 'package:share_plus/share_plus.dart';
-import 'package:cross_file/cross_file.dart';
-import 'package:gal/gal.dart';
-
-
-import 'package:permission_handler/permission_handler.dart';
-import 'package:android_intent_plus/android_intent.dart';
-import 'package:android_intent_plus/flag.dart';
-
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
+  // Ensure plugin services are initialized
   WidgetsFlutterBinding.ensureInitialized();
-  final cameras = await availableCameras();
-  runApp(MyApp(cameras: cameras));
+  
+  runApp(const FullPassApp());
 }
 
-class MyApp extends StatelessWidget {
-  final List<CameraDescription> cameras;
-
-  const MyApp({Key? key, required this.cameras}) : super(key: key);
+class FullPassApp extends StatelessWidget {
+  const FullPassApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CameraHome(cameras: cameras),
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+      home: const FileOutputScreen(),
     );
   }
 }
 
-class CameraHome extends StatefulWidget {
-  final List<CameraDescription> cameras;
-
-  const CameraHome({Key? key, required this.cameras}) : super(key: key);
+class FileOutputScreen extends StatefulWidget {
+  const FileOutputScreen({super.key});
 
   @override
-  _CameraHomeState createState() => _CameraHomeState();
+  State<FileOutputScreen> createState() => _FileOutputScreenState();
 }
 
-class _CameraHomeState extends State<CameraHome> {
-  late CameraDescription selectedCamera;
-  String selectedFigure = 'circle'; // Default selection is circle
-  final TextEditingController centerXController = TextEditingController();
-  final TextEditingController centerYController = TextEditingController();
-  final TextEditingController radiusController = TextEditingController();
-  final TextEditingController upperLeftXController = TextEditingController();
-  final TextEditingController upperLeftYController = TextEditingController();
-  final TextEditingController widthController = TextEditingController();
-  final TextEditingController heightController = TextEditingController();
+class _FileOutputScreenState extends State<FileOutputScreen> {
+  String _status = "Initializing...";
+  String _filePath = "";
 
   @override
   void initState() {
     super.initState();
-    selectedCamera = widget.cameras.first;
+    _writeFullPass();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Pick the camera, choose figure type and enter coordinates')),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              DropdownButton<CameraDescription>(
-                value: selectedCamera,
-                onChanged: (CameraDescription? newCamera) {
-                  if (newCamera != null) {
-                    setState(() {
-                      selectedCamera = newCamera;
-                    });
-                  }
-                },
-                items: widget.cameras.map((camera) {
-                  return DropdownMenuItem<CameraDescription>(
-                    value: camera,
-                    child: Text(
-                      camera.lensDirection == CameraLensDirection.front
-                          ? 'Front Camera'
-                          : 'Rear Camera',
-                    ),
-                  );
-                }).toList(),
-              ),
-              FigurePicker(
-                selectedFigure: selectedFigure,
-                onFigureChanged: (String value) {
-                  setState(() {
-                    selectedFigure = value;
-                  });
-                },
-              ),
-              if (selectedFigure == 'circle') ...[
-                const Text('Center X:'),
-                TextField(controller: centerXController, keyboardType: TextInputType.number),
-                const Text('Center Y:'),
-                TextField(controller: centerYController, keyboardType: TextInputType.number),
-                const Text('Radius:'),
-                TextField(controller: radiusController, keyboardType: TextInputType.number),
-              ] else if (selectedFigure == 'rectangle') ...[
-                const Text('Upper Left X:'),
-                TextField(controller: upperLeftXController, keyboardType: TextInputType.number),
-                const Text('Upper Left Y:'),
-                TextField(controller: upperLeftYController, keyboardType: TextInputType.number),
-                const Text('Width:'),
-                TextField(controller: widthController, keyboardType: TextInputType.number),
-
-                const Text('Height:'),
-                TextField(controller: heightController),
-              ],
-              const SizedBox(height: 20),
-              ElevatedButton(
-                child: const Text('Open'),
-                onPressed: () {
-                  final figureData = {
-                    'figure': selectedFigure,
-                    'centerX': double.tryParse(centerXController.text) ?? 0.0,
-                    'centerY': double.tryParse(centerYController.text) ?? 0.0,
-                    'radius': double.tryParse(radiusController.text) ?? 50.0,
-                    'upperLeftX': double.tryParse(upperLeftXController.text) ?? 100.0,
-                    'upperLeftY': double.tryParse(upperLeftYController.text) ?? 100.0,
-                    'width': double.tryParse(widthController.text) ?? 100.0,
-                    'height': double.tryParse(heightController.text) ?? 100.0,
-                  };
-
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => CameraPreviewScreen(
-                        camera: selectedCamera,
-                        figureData: figureData,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class FigurePicker extends StatelessWidget {
-  final String selectedFigure;
-  final Function(String) onFigureChanged;
-
-  const FigurePicker({
-    Key? key,
-    required this.selectedFigure,
-    required this.onFigureChanged,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Radio<String>(
-          value: 'circle',
-          groupValue: selectedFigure,
-          onChanged: (String? value) {
-            if (value != null) onFigureChanged(value);
-          },
-        ),
-        const Text('Circle'),
-        Radio<String>(
-          value: 'rectangle',
-          groupValue: selectedFigure,
-          onChanged: (String? value) {
-            if (value != null) onFigureChanged(value);
-          },
-        ),
-        const Text('Rectangle'),
-      ],
-    );
-  }
-}
-
-class CameraPreviewScreen extends StatefulWidget {
-  final CameraDescription camera;
-  final Map<String, Object> figureData;
-
-  const CameraPreviewScreen({
-    Key? key,
-    required this.camera,
-    required this.figureData,
-  }) : super(key: key);
-
-  @override
-  _CameraPreviewScreenState createState() => _CameraPreviewScreenState();
-}
-
-class _CameraPreviewScreenState extends State<CameraPreviewScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-  bool isRecording = false;
-  late String videoPath;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = CameraController(widget.camera, ResolutionPreset.veryHigh);
-    _initializeControllerFuture = _controller.initialize();
-	_requestPermissions();
-  }
-  Future<void> _requestPermissions() async {
-    Map<Permission, PermissionStatus> statuses = await [
-      Permission.camera,
-      Permission.microphone,
-      Permission.photos, // Request media access on Android 13+
-      Permission.videos, // For video media access on Android 13+
-    ].request();
-
-    if (await Permission.camera.isPermanentlyDenied ||
-      await Permission.microphone.isPermanentlyDenied ||
-      await Permission.photos.isPermanentlyDenied ||
-      await Permission.videos.isPermanentlyDenied) {
-        await openAppSettings();
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  Future<void> _toggleRecording() async {
+  Future<void> _writeFullPass() async {
     try {
-		  // Check if all required permissions are granted
-    bool hasPermissions = await Permission.camera.isGranted &&
-        await Permission.microphone.isGranted;
+      // 1. Get the app's internal data directory
+      final directory = await getApplicationDocumentsDirectory();
+      
+      // 2. Define the file path
+      final file = File('${directory.path}/fullpass.txt');
 
+      // 3. Write the content
+      await file.writeAsString('fullpass');
 
-    if (isRecording) {
-            final XFile xf = await _controller.stopVideoRecording();
-			final File file = File(xf.path);
-			final newFileName = file.path.replaceAll('.temp', '.mp4');
-			final renamedFile = await file.rename(newFileName);
-			setState(() {
-			  isRecording = false;
-			  videoPath = renamedFile.path;
-			});
-			await Gal.requestAccess();
-			await Gal.putVideo('$videoPath', album: 'alb1');
-			final params = ShareParams(
-				text: 'Result',
-				files: [XFile(videoPath)],
-			);
-			await SharePlus.instance.share(params);
-
-
-	  } else {
-		final directory = await getApplicationDocumentsDirectory();
-		final videoFile = '${directory.path}/${DateTime.now().millisecondsSinceEpoch}.mp4';
-		await _controller.startVideoRecording();
-		setState(() {
-		  isRecording = true;
-		  videoPath = videoFile;
-		});
-	  }
-		} catch (e) {
-		  ScaffoldMessenger.of(context).showSnackBar(
-			SnackBar(content: Text('Error during recording: $e')),
-		  );
-		}
-	}
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Stack(
-              fit: StackFit.expand,
-              children: [
-                CameraPreview(_controller),
-                CustomPaint(
-                   painter: FigurePainter(
-					   figureData: widget.figureData,
-					   previewSize: _controller.value.previewSize!,
-				   ),
-                ),
-                Positioned(
-                  bottom: 20,
-                  right: 20,
-                  child: IconButton(
-                    icon: Icon(
-                      isRecording ? Icons.stop : Icons.videocam,
-                      color: Colors.red,
-                      size: 57,
-                    ),
-                    onPressed: _toggleRecording,
-                  ),
-                ),
-              ],
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    );
-  }
-}
-
-class FigurePainter extends CustomPainter {
-  final Map<String, Object> figureData;
-  final Size previewSize; // Add preview size as an input
-
-  FigurePainter({required this.figureData, required this.previewSize});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final Paint paint = Paint()
-      ..color = Colors.blue // Border color
-      ..style = PaintingStyle.stroke  // Ensure only the border is drawn
-      ..strokeWidth = 4.0; // Border width, can be adjusted
-
-    final String? figureType = figureData['figure'] as String?;
-
-    // Calculate scale factors based on the actual preview size
-    double scaleX = size.width / previewSize.width;
-    double scaleY = size.height / previewSize.height;
-
-    if (figureType == 'circle') {
-      final double centerX = figureData['centerX'] as double? ?? 0.0;
-      final double centerY = figureData['centerY'] as double? ?? 0.0;
-      final double radius = figureData['radius'] as double? ?? 50.0;
-
-      // Scale coordinates based on the preview size
-      final scaledCenterX = centerX * scaleX;
-      final scaledCenterY = centerY * scaleY;
-      final scaledRadius = radius * scaleX; // Assuming the circle scales uniformly
-
-      canvas.drawCircle(Offset(scaledCenterX, scaledCenterY), scaledRadius, paint);
-    } else if (figureType == 'rectangle') {
-      final double upperLeftX = figureData['upperLeftX'] as double? ?? 0.0;
-      final double upperLeftY = figureData['upperLeftY'] as double? ?? 0.0;
-      final double width = figureData['width'] as double? ?? 100.0;
-      final double height = figureData['height'] as double? ?? 100.0;
-
-      // Scale coordinates based on the preview size
-      final scaledUpperLeftX = upperLeftX * scaleX;
-      final scaledUpperLeftY = upperLeftY * scaleY;
-      final scaledWidth = width * scaleX;
-      final scaledHeight = height * scaleY;
-
-      Rect rect = Rect.fromLTWH(scaledUpperLeftX, scaledUpperLeftY, scaledWidth, scaledHeight);
-      canvas.drawRect(rect, paint);
+      setState(() {
+        _filePath = file.path;
+        _status = "Success! 'fullpass' written to:";
+      });
+      
+      print("File written to: ${file.path}");
+    } catch (e) {
+      setState(() {
+        _status = "Error: $e";
+      });
     }
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Data Directory Output")),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              _status.contains("Success") ? Icons.check_circle : Icons.error,
+              size: 64,
+              color: _status.contains("Success") ? Colors.green : Colors.red,
+            ),
+            const SizedBox(height: 20),
+            Text(_status, style: const TextStyle(fontWeight: FontWeight.bold)),
+            const SizedBox(height: 10),
+            SelectableText(
+              _filePath,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontFamily: 'monospace', fontSize: 12),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
